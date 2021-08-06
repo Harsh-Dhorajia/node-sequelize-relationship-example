e5d9ffeda1b96252abb9d855bf950dcbe47ab419
harsh.dhorajia <harsh.d@logicwind.co>
2021-08-06 12:33:10 +0530

    sequelize relationship example code

---
+++ sequelize-assoc/models/role.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Role.init({
+    role_name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  Role.associate = function (models) {
+    Role.belongsToMany(models.User, {
+      through: 'UserRole',
+      as: 'users',
+      foreignKey: 'role_id'
+    });
+  };
+  return Role;
+};
